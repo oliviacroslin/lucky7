124e9cf953eff24e5f8ef2bbd1a022f98afcfe5c
oliviacroslin <59617089+oliviacroslin@users.noreply.github.com>
2020-01-14 09:52:03 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,9 +10,23 @@ int main() {
 
     cout << "Enter 5 integers: ";
     cin >> a >> b >> c >> d >> e;
-
     cout << "The numbers are " << a << " " << b << " " << c << " " << d << " " << e  << endl;
 
+    int max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    if (d > max) max = d;
+    if (e > max) max = e;
+
+    cout << "The max value is " << max << endl;
+
+    int min = e;
+    if (a < min) min = a;
+    if (b < min) min = b;
+    if (c < min) min = c;
+    if (d < min) min = d;
+
+    cout << "The min value is " << min << endl;
 
     return 0;
 }
