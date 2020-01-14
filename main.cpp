1fd4c108f3c7cc477c46f9663bcebe194b68a92f
oliviacroslin <59617089+oliviacroslin@users.noreply.github.com>
2020-01-14 09:29:35 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// Olivia Croslin
+// This is my own work
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a,b,c,d,e;
+
+    cout << "Enter 5 integers: ";
+    cin >> a >> b >> c >> d >> e;
+
+    cout << "The numbers are " << a << " " << b << " " << c << " " << d << " " << e  << endl;
+
+
+    return 0;
+}
